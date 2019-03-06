dd3b8335144233b8d91b4153e2387a96c34732ee
golda <goldaev@gmail.com>
2019-03-06 12:13:06 +0300

    неправильно расставляются числа

--- app/src/main/java/com/golda/app/pvttraining/dz4/Diagram.java
+++ app/src/main/java/com/golda/app/pvttraining/dz4/Diagram.java
@@ -9,19 +9,17 @@ import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.View;
 
-import java.util.Calendar;
+import java.util.Random;
 
 public class Diagram extends View {
     private int height, width = 0;
     private int padding = 0;
     private int fontSize = 0;
-    private int numeralSpacing = 0;
-    private int radius = 0;
     private Paint paint;
     private boolean isInit;
-    private int[] numbers = {5,5,10};
-    private int[] percent;
-    private RectF rectf = new RectF();
+    private int[] numbers = {5, 10, 5};
+    private RectF rectf;
+    private Rect rect = new Rect();
 
     public Diagram(Context context) {
         super(context);
@@ -40,44 +38,54 @@ public class Diagram extends View {
         if (!isInit) {
             initDiagram();
         }
-        calculatePercent(numbers);
         drawDiagram(canvas);
-        postInvalidateDelayed(500);
-        invalidate();
+
     }
 
     private void initDiagram() {
         height = getHeight();
         width = getWidth();
-        padding = numeralSpacing + 50;
+        padding = 50;
         fontSize = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 13,
                 getResources().getDisplayMetrics());
         int min = Math.min(height, width);
-        radius = min / 2 - padding;
         paint = new Paint();
         isInit = true;
     }
 
-    private void calculatePercent(int[] numbers) {
-        percent = new int[numbers.length];
-        int sum = 0;
-        for (int number : numbers) {
-            sum = sum + number;
-        }
-        for (int i = 0; i < percent.length; i++) {
-            percent[i] = 360 / sum * numbers[i];
-        }
+    private void getRandomColor() {
+        Random rnd = new Random();
+        paint.setARGB(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
     }
 
     private void drawDiagram(Canvas canvas) {
         paint.reset();
-        paint.setColor(getResources().getColor(android.R.color.black));
-        paint.setStrokeWidth(5);
-        paint.setStyle(Paint.Style.STROKE);
         paint.setAntiAlias(true);
-        canvas.drawArc(rectf, 90, 270, true, paint);
+        rectf = new RectF(padding,padding, width-padding, height-padding);
+        float sumArray = 0;
+        float startAngle = 0;
+        for (int ma : numbers) {
+            sumArray += ma;
+        }
+
+        for (int number : numbers) {
+            getRandomColor();
+            float angle = 360 / sumArray * number;
+            canvas.drawArc(rectf, startAngle, angle, true, paint);
+            float angle1 = startAngle + angle / 2;
+            drawText(canvas, angle1, number );
+            startAngle += angle;
+        }
+
     }
 
+    private void drawText(Canvas canvas, double angle, int number) {
+        paint.setTextSize(fontSize);
+        String tmp = String.valueOf(number);
+        paint.getTextBounds(tmp, 0, tmp.length(), rect);
+        int x = (int) (width / 2 + Math.cos(angle) * (width/2 - padding/2) - rect.width() / 2);
+        int y = (int) (height / 2 + Math.sin(angle) * (height/2 - padding/2) + rect.height() / 2);
+        canvas.drawText(tmp, x, y, paint);
 
-
+    }
 }
