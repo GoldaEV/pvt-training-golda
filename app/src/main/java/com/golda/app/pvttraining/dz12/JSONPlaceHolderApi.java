dc25467c2dd9056ae9708365b8101ece153132cc
Evgeniy Golda <goldaev@gmail.com>
2019-04-18 23:18:12 +0300

    add retrofit

--- app/src/main/java/com/golda/app/pvttraining/dz12/JSONPlaceHolderApi.java
+++ app/src/main/java/com/golda/app/pvttraining/dz12/JSONPlaceHolderApi.java
@@ -13,19 +13,20 @@ import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
 public interface JSONPlaceHolderApi {
+    String DB_URL = "FD7FE946-1DEE-555C-FF23-B73859B3D800/E862840C-2D42-A2F2-FF12-8F4FC6146800/data/Person";
 
-    @GET("FD7FE946-1DEE-555C-FF23-B73859B3D800/E862840C-2D42-A2F2-FF12-8F4FC6146800/data/Person")
-    public Call<ArrayList<Person>> getPersons();
+    @GET(DB_URL)
+    Call<ArrayList<Person>> getPersons();
 
-    @GET("FD7FE946-1DEE-555C-FF23-B73859B3D800/E862840C-2D42-A2F2-FF12-8F4FC6146800/data/Person/{objectId}")
-    public Call<Person> getPersonByID(@Path("objectId") String objectId);
+    @GET(DB_URL + "/{objectId}")
+    Call<Person> getPersonByID(@Path("objectId") String objectId);
 
-    @POST("FD7FE946-1DEE-555C-FF23-B73859B3D800/E862840C-2D42-A2F2-FF12-8F4FC6146800/data/Person")
-    public Call<Person> createPerson(@Body Person person);
+    @POST(DB_URL)
+    Call<Person> createPerson(@Body Person person);
 
-    @DELETE("FD7FE946-1DEE-555C-FF23-B73859B3D800/E862840C-2D42-A2F2-FF12-8F4FC6146800/data/Person/{objectId}")
-    public Call<Person> removePerson(@Path("objectId") String objectId);
+    @DELETE(DB_URL + "/{objectId}")
+    Call<Person> removePerson(@Path("objectId") String objectId);
 
-    @PUT("FD7FE946-1DEE-555C-FF23-B73859B3D800/E862840C-2D42-A2F2-FF12-8F4FC6146800/data/Person/{objectId}")
-    public Call<Void> editStudent(@Path("objectId") String objectId, @Body Person student);
+    @PUT(DB_URL + "/{objectId}")
+    Call<Void> editStudent(@Path("objectId") String objectId, @Body Person student);
 }
